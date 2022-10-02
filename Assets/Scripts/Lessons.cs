be3f113d679fe29f9bb10d6718a4c58995c32412
Mamay-ka <mamay_ka@mail.ru>
2022-10-02 12:55:42 +0300

    ДЗ 5
    Установите A* Pathfiding Project. Настройте простого врага только с помощью представленных в нём компонентов.
    
    Создайте противника, патрулирующего зону. Противник должен преследовать игрока в своей зоне, если увидел его.

--- Assets/Scripts/Lessons.cs
+++ Assets/Scripts/Lessons.cs
@@ -1,15 +1,15 @@
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
-using System;
 
 public class Lessons : MonoBehaviour
 {
-    //хотим сделать парралакс. Создадим два поля
+    
     [SerializeField]
-    private Camera _camera; //положение камеры
+    private Camera _camera; 
 
     [SerializeField]
-    private SpriteRenderer _background;//здесь будет бэкграунд/фон
+    private SpriteRenderer _background;
 
     [SerializeField]
     private CharacterView _characterView;
@@ -29,11 +29,23 @@ public class Lessons : MonoBehaviour
     [SerializeField]
     private List<LevelObjectView> _levelObjectViews;//!
 
+    [SerializeField]
+    private AIConfig _config;
+
+    [SerializeField]
+    private EnemyView _enemyView;
+
+    [Header("Protector AI")]
+    [SerializeField] private AIDestinationSetter _protectorAIDestinationSetter;
+    [SerializeField] private AIPatrolPath _protectorAIPatrolPath;
+    [SerializeField] private LevelObjectTrigger _protectedZoneTrigger;
+    [SerializeField] private Transform[] _protectorWaypoints;
+
     private LevelCompleteManager _levelCompleteManager;//!
 
     private CoinsManager _coinsManager;
                 
-    private ParalaxManager _paralaxManager;//создадим объект класса ParalaxManager 
+    private ParalaxManager _paralaxManager;
 
     private SpriteAnimator _spriteAnimator;
 
@@ -45,44 +57,59 @@ public class Lessons : MonoBehaviour
 
     private BulletsEmitter _bulletsEmitter;
 
-    //далее инициализируем все контроллеры в методе Старт
+    private SimplePatrolAI _simplePatrolAI;
+
+    private ProtectorAI _protectorAI;
+    private ProtectedZone _protectedZone;
+
+
     private void Start()
     {
         _levelCompleteManager = new LevelCompleteManager(_levelObjectViews);//!
         _coinsManager = new CoinsManager(_coinViews);
-        _paralaxManager = new ParalaxManager(_camera, _background.transform);//инициализируем данное поле. Создаем конструктор и перемещаем на вход камеру
+        _paralaxManager = new ParalaxManager(_camera, _background.transform);
         _spriteAnimator = new SpriteAnimator(_spriteAnimationConfig);
         //_spriteAnimator.StartAnimation(_characterView.SpriteRenderer, Track.walk, true, 10);
         //_mainHeroWalker = new MainHeroWalker(_characterView, _spriteAnimator);//создали объект
         _mainPhysicsHeroWalker = new MainHeroPhysicsWalker(_characterView, _spriteAnimator);
         _aimingMuzzle = new AimingMuzzle(_cannonView.transform, _characterView.transform);
         _bulletsEmitter = new BulletsEmitter(_bullets, _cannonView.MuzzleTransform);
+        //_simplePatrolAI = new SimplePatrolAI(_enemyView, new SimplePatrolAIModel(_config));
 
+        _protectorAI = new ProtectorAI(_characterView, new PatrolAIModel(_protectorWaypoints), _protectorAIDestinationSetter, _protectorAIPatrolPath);
+        _protectorAI.Init();
+
+        _protectedZone = new ProtectedZone(_protectedZoneTrigger, new List<IProtector> { _protectorAI });
+        _protectedZone.Init();
     }
 
-    //в методе апдэйт будет крутиться логика
+    
     private void Update()
     {
         
-        _paralaxManager.Update();//теперь конструктор в Апдейте нужно обновлять каждый раз.(взяли Update из класса ParalaxManager )
+        _paralaxManager.Update();
         _spriteAnimator.Update();
 
-        //_mainHeroWalker.Update();//запустили Апдэйт из данного кoнтроллера
+        //_mainHeroWalker.Update();
         _aimingMuzzle.Update();
         _bulletsEmitter.Update();
     }
 
-    //для движения персонажа при помощи физики понадобится фикседАпдэйт
+    
     private void FixedUpdate()
     {
         _mainPhysicsHeroWalker.FixedUpdate();
+        //_simplePatrolAI.FixedUpdate();
     }
 
     //чтобы чистить наши контроллеры
     private void OnDestroy()
     {
         _coinsManager.Dispose();
-        _levelCompleteManager.Dispose();//!
+        //_levelCompleteManager.Dispose();//!
+
+        _protectorAI.Deinit();
+        _protectedZone.Deinit();
     }
 }
 
